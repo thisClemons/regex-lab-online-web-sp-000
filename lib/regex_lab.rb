6b293443a3a8df98c058c9c4dcb387ba3d72a5dd
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-17 21:24:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,10 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*[\.?!]$/)
-    return true
-  else return false
-  end
+  text.match(/^[A-Z].*[\.?!]$/)? true: false
 end
 
 def valid_phone_number?(phone)
